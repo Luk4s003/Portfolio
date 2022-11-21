79c189d631ad10fd86e28b7e7d80d6ef69abffbc
Lukas Reis de Oliveira <lukas.oliveira@csprime.com.br>
2022-11-21 00:37:05 -0300

    Atualização de linguagem utilizada

--- src/components/Foto.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import foto from "../img/FotoSocial.png";
-import "./style/foto.css";
-
-export default (props) => (
-    <div className="caixaFoto">
-        <img src={foto} alt="Foto do Lukas" />
-    </div>
-);
